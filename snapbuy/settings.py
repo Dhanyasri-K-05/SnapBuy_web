1314d0ad6a4b059b5ead6d2d679b10bd852f60db
Dhanyasri-K-05 <155838488+Dhanyasri-K-05@users.noreply.github.com>
2025-03-01 17:13:49 +0530

    Update settings.py

--- snapbuy/settings.py
+++ snapbuy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uxg2g$fz^1w!@67gr(qoucpae(tg)(lk661u@f#w+#+mjc9c7@'
+SECRET_KEY = '#####################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'snapbuy.wsgi.application'
         'NAME': 'website',
         'HOST':'localhost',
         'USER':'root',
-        'PASSWORD':'tiger',
+        'PASSWORD':'######',
         'PORT':'3306'
     }
 }"""
